565f1dee6a94cbdc3aa5eb4e385e1138ca655aaf
mallory-jpg <65197541+mallory-jpg@users.noreply.github.com>
2021-11-06 12:01:26 -0500

    initial commit

---
+++ contracts/NFTGame.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract NFTGame {
+  constructor() {
+    console.log("THIS IS MY GAME CONTRACT. NICE.");
+  }
+}
